2016e964544fe5af25ee5010cd01b732df353ee3
Marlon <martianworm17@gmail.com>
2017-09-17 09:42:04 +0800

    Add htmlforms.php

---
+++ htmlforms.php
@@ -0,0 +1,25 @@
+<?php 
+if(isset($_POST["name"]) || isset($_POST["age"])) {
+        if (preg_match("/[^A-Za-z'-]/",$_POST['name'])) {
+          die ("Invalid name and name should be alpha");  
+        }
+    
+    	  echo "Welcome ". $_POST['name'] ."<br />";
+    	  echo "You are ". $_POST['age']  ." years old.";
+        
+    	  exit();
+    }		
+?>
+<html>
+	<head>
+		<title>PHP HTML Forms</title>
+	</head>
+	<body>
+		<form action = "<?php $_PHP_SELF ?>" method = "POST">
+			Name: <input type="text" name="name" />
+			<br />
+			Age: <input type="text" name="age" />
+			<input type = "submit" />
+		</form>
+	</body>
+</html>
