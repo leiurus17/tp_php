0a92d95074dcbcd05bf399d9309b7c239c19f352
Marlon <martianworm17@gmail.com>
2017-09-17 21:39:32 +0800

    PHP - Cookies sample files

---
+++ cookies/setcookies.php
@@ -0,0 +1,14 @@
+<?php
+    setcookie("name", "Uzumaki Naruto", time()+3600, "/", "", 0);
+    setcookie("age", "13", time()+3600, "/", "", 0);
+?>
+<html>
+	<head>
+		<title>Setting Cookies with PHP</title>
+	</head>
+	<body>
+		<?php 
+		  echo "Set Cookies.";
+		?>
+	</body>
+</html>
