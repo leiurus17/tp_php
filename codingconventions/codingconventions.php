3656fb564334ad26cc6835a32e5777944733d512
Marlon <martianworm17@gmail.com>
2017-09-21 14:32:39 +0800

    PHP - Conding Conventions

---
+++ codingconventions/codingconventions.php
@@ -0,0 +1,66 @@
+<html>
+	<head>
+		<title>PHP Coding Standards Example</title>
+	</head>
+
+	<body>
+
+		<?php
+			
+		/*	
+			If statements
+		
+			if ((condition1) || (condition2)) {
+			   action1;
+			}elseif ((condition3) && (condition4)) {
+			   action2;
+			}else {
+			   default action;
+			}
+        */	
+
+		/*
+			Switch statements
+		
+		switch (condition) {
+		   case 1:
+			  action1;
+			  break;
+		   
+		   case 2:
+			  action2;
+			  break;
+				 
+		   default:
+			  defaultaction;
+			  break;
+		}
+		
+		*/
+		
+		/*
+			Functions
+			
+			function fooFunction($arg1, $arg2 = '') {
+				if (condition) {
+					statement;
+				}
+				return $val;
+			}
+		
+		*/
+		
+		/*
+			Variable Names
+			
+			Use all lower case letters
+			Use '_' as the word separator.
+			Global variables should be prepended with a 'g'.
+			Global constants should be all caps with '_' separators.
+			Static variables may be prepended with 's'.
+	    
+		*/
+		
+		?>
+	</body>
+</html>
