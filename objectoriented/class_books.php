801d7f56c3edd63e5f377464c9f19c84ccd3d4cf
Marlon <martianworm17@gmail.com>
2017-10-21 09:10:16 +0800

    Defining PHP Classes

---
+++ objectoriented/class_books.php
@@ -0,0 +1,26 @@
+<?php 
+    class Books {
+        /* Member variables */
+        var $price;
+        var $title;
+        
+        /* Member functions */
+        function setPrice($par) {
+            $this -> price = $par;
+        }
+        
+        function getPrice() {
+            echo $this -> price ."<br />";
+        }
+        
+        function setTitle($par) {
+            $this -> title = $par;
+        }
+        
+        function getTitle() {
+            echo $this -> title ."<br />";
+        }
+    }
+
+
+?>
